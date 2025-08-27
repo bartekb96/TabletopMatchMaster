2e7f82b827d05eca7b69956d627e91d283bd27ec
Bartosz Bednarek <b.bednarek@captron.com>
2025-08-27 09:39:04 +0200

    initialize node model

---
+++ TabletopMatchMaster.Logic/Models/Node.cs
@@ -0,0 +1,21 @@
+﻿namespace TabletopMatchMaster.Logic.Models
+{
+	internal class Node
+	{
+		public string Name { get; set; }
+
+		public string? BranchName { get; set; }
+
+		public Node? ParentNode { get; set; }
+
+		public Node(
+			string name,
+			string? branchName,
+			Node? parent)
+		{
+			Name = name;
+			BranchName = branchName;
+			ParentNode = parent;
+		}
+	}
+}
